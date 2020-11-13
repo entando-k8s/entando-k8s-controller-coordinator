2cbfb53e0a2c143c181705feb8d285a16462e431
Ampie Barnard <ampie.private@gmail.com>
2020-11-13 11:12:00 +0200

    ENG-1447 fixed the copy-images.sh script

--- src/main/bash/copy-images.sh
+++ src/main/bash/copy-images.sh
@@ -2,16 +2,15 @@ version=$1
 registry=$2
 curl -o versions.yaml https://raw.githubusercontent.com/entando-k8s/entando-k8s-controller-coordinator/v${version}/charts/entando-k8s-controller-coordinator/templates/docker-image-info-configmap.yaml
 
-#docker pull entando/entando-k8s-controller-coordinator:${version}
-#docker tag  entando/entando-k8s-controller-coordinator:${version} ${registry}/entando/entando-k8s-controller-coordinator:${version}
-#docker push ${registry}/entando/entando-k8s-controller-coordinator:${version}
+docker pull entando/entando-k8s-controller-coordinator:${version}
+docker tag  entando/entando-k8s-controller-coordinator:${version} ${registry}/entando/entando-k8s-controller-coordinator:${version}
+docker push ${registry}/entando/entando-k8s-controller-coordinator:${version}
 echo "entando/entando-k8s-controller-coordinator:${version}"
 keys=$(cat versions.yaml| yq r - 'data'  | yq r - --printMode p  '*')
 for key in $keys; do
     value=$(cat versions.yaml| yq r - "data.${key}")
     version=$( echo $value | yq r - "version")
-    echo "entando/${key}:${version}"
-#    docker pull entando/${key}:${version}
-#    docker tag  entando/${key}:${version} ${registry}/entando/${key}:${version}
-#    docker push ${registry}/entando/${key}:${version}
+    docker pull entando/${key}:${version}
+    docker tag  entando/${key}:${version} ${registry}/entando/${key}:${version}
+    docker push ${registry}/entando/${key}:${version}
 done
